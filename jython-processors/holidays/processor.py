9c0f9a949aed3f7a208423a5d725adb5e7dfd857
Liev Garcia <liev.garcia@gmail.com>
2019-12-03 14:57:06 +0000

    processing changes + weekends support

--- jython-processors/holidays/processor.py
+++ jython-processors/holidays/processor.py
@@ -3,42 +3,57 @@ import os
 import pandas as pd
 from dateutil import parser
 import datetime
-
+import json
 
 logging.basicConfig(level=logging.INFO,  format='Holidays plugin %(levelname)s - %(message)s')
-
 folders = ['installed', 'dev']
-for folder in folders:
-    file_path = os.path.join(os.environ['DIP_HOME'], 'plugins/{}/holidays/resources/holidays_calendar.csv'.format(folder))
-    if os.path.exists(file_path) is True:
-        df = pd.read_csv(file_path)
-        df['date'] = df['date'].astype('datetime64')
 
-# TODO maybe a better way of handling the case where the resources file is not found in either folder.
-# Like if someone forgot to upload it to git(?)
-try:
-    df
-except Exception as e:
-    logging.info("Creating an empty dataframe as file was not found.")
-    df = pd.DataFrame(columns=['country_iso', 'date', 'holiday_reason'])
+
+def get_file_path(file_name):
+    for folder in folders:
+        file_path = os.path.join(os.environ['DIP_HOME'], 'plugins/{}/holidays/resources/{}'.format(folder, file_name))
+        if os.path.exists(file_path) is True:
+            return file_path
+    return None
+
+
+def load_or_create_dataframe(file_name, columns):
+
+    file_path = get_file_path(file_name)
+    if file_path is None:
+        df = pd.DataFrame(columns=columns, index_col=index_col)
+        logging.info("Creating an empty dataframe as file was not found.")
+    else:
+        df = pd.read_csv(file_path)
+    return df
+
+
+holidays_df = load_or_create_dataframe("holidays_calendar.csv", columns=["country_name","country_iso","date","holiday_reason"])
+holidays_df['date'] = holidays_df['date'].astype('datetime64')
+weekends_df = load_or_create_dataframe("weekends.csv", columns=["country_name","country_iso","weekend_day_numbers"])
 
 
 def process(row):
     input_column = params.get('input_column')
     country = params.get('country')
     output_holiday_name = params.get('output_holiday_name')
+    flag_weekends = params.get('flag_weekends')
 
     try:
         parsed_input = parser.parse(row.get(input_column), ignoretz=True)
+        day_of_the_week = parsed_input.weekday()
     except Exception as e:
         parsed_input = pd.Timestamp.min
         logging.info("Error parsing input date, using dummy date.")
 
-    is_holiday = df[(df['country_iso']==country) & (df['date']==parsed_input)]
+    is_holiday = holidays_df[(holidays_df.country_iso==country) & (holidays_df.date==parsed_input)]
 
+    row["is_holiday"] = bool(is_holiday.shape[0])
     if output_holiday_name is True:
-        row["is_holiday"] = bool(is_holiday.shape[0])
         row["holiday_name"] = next(iter(is_holiday.holiday_reason.tolist()), None)
-    else:
-        row["is_holiday"] = bool(is_holiday.shape[0])
+
+    if flag_weekends is True:
+        weekend_days = json.loads(next(iter(weekends_df.weekend_day_numbers.tolist()), None))
+        row["is_weekend"] = day_of_the_week in weekend_days
+
     return row
